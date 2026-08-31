ec75e6e572940ed33b6783028e8be9e9493225a5
Ian Maciel <ianmaciel76@gmail.com>
2026-08-31 08:25:34 -0300

    fix: type editor input handler

--- src/components/block-editor.tsx
+++ src/components/block-editor.tsx
@@ -4,6 +4,7 @@ import Placeholder from "@tiptap/extension-placeholder";
 import TaskItem from "@tiptap/extension-task-item";
 import TaskList from "@tiptap/extension-task-list";
 import { Markdown } from "@tiptap/markdown";
+import type { EditorView } from "@tiptap/pm/view";
 import { EditorContent, useEditor } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
 import { CopyIcon, DownloadIcon } from "lucide-react";
@@ -340,7 +341,7 @@ function BlockEditor({
     () => ({
       attributes,
       handleDOMEvents: {
-        input: (view) => {
+        input: (view: EditorView) => {
           scheduleCommit({
             schemaVersion: BLOCK_EDITOR_DOCUMENT_SCHEMA_VERSION,
             doc: view.state.doc.toJSON() as BlockEditorDocument["doc"],
