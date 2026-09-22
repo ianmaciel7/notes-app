fea6e7db5d6fb5389623434f468d1df4bc0f09bc
Ian Maciel <ianmaciel76@gmail.com>
2026-09-21 21:25:09 -0300

    feat: add GoogleSignInButton component

--- src/components/recall/google-sign-in-button.tsx
+++ src/components/recall/google-sign-in-button.tsx
@@ -3,7 +3,13 @@ import { Button } from "@/components/ui/button";
 
 export function GoogleIcon(props: React.SVGProps<SVGSVGElement>) {
   return (
-    <svg viewBox="0 0 24 24" width="16" height="16" {...props}>
+    <svg
+      viewBox="0 0 24 24"
+      width="16"
+      height="16"
+      aria-hidden="true"
+      {...props}
+    >
       <path
         d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92c-.26 1.37-1.04 2.53-2.21 3.31v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.09z"
         fill="#4285F4"
